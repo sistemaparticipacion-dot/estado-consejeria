c9f80037593328dcb3eef5034face226cce829d7
sistemaparticipacion-dot <sistemaparticipacion@scrd.gov.co>
2026-04-10 08:40:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -210,13 +210,13 @@ function generarCertificado() {
     // ===============================
     // 6. NOTA AL PIE
     // ===============================
-    doc.setFontSize(6);
+    doc.setFontSize(5);
     doc.setTextColor(100, 100, 100);
 
     const nota = "Nota: Este certificado ha sido generado automáticamente desde el portal web Radar Cultural. Puede verificar la autenticidad del mismo a través del correo sistemaparticipacion@scrd.gov.co";
 
     const notaLines = doc.splitTextToSize(nota, textWidth);
-    doc.text(notaLines, margin, 290);
+    doc.text(notaLines, margin, 220);
 
     // ===============================
     // EXPORTAR
